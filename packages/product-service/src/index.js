24cc990143b565c5667843390948aa4c2461234f
hehk <kyle1101000@gmail.com>
2017-12-21 20:47:02 +0000

    initial docker setup

--- packages/product-service/src/index.js
+++ packages/product-service/src/index.js
@@ -1,5 +1,6 @@
 const { graphql, buildSchema } = require('graphql');
 const { GraphQLServer } = require('graphql-yoga');
+const redis = require('redis');
 
 const types = require('./types');
 const resolvers = require('./resolvers');
@@ -15,6 +16,9 @@ const server = new GraphQLServer({
   resolvers,
   options,
 });
+
+const db = redis.createClient();
+db.on('error', err => console.log(`Error ${err}`));
 
 server.start(_ => console.log(`
 endpoint:   localhost:${options.port}${options.endpoint}
