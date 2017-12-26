1cf7675d42186dc6af95ebf7234bd1198dd590af
hehk <kyle1101000@gmail.com>
2017-12-26 18:44:35 -0500

    moved loading page and scraping into different functions

--- packages/scraper/src/index.js
+++ packages/scraper/src/index.js
@@ -1,51 +1,41 @@
-// const cheerio = require('cheerio');
-// const request = require('request');
-
-// const getAuctions = () => {
-//   request('http://bidfta.bidqt.com/BidFTA/#/Main', (error, response, html) => {
-//     console.log(html);
-//     const $ = cheerio.load(html);
-//     console.log($('.app-list-item'))
-
-//     console.log($('.app-list-item').map((i, el) => {
-//       const link = el.find('.app-card-footer a').getAttribute('href');
-//       return "test"
-//     }).get())
-//   });
-// }
-
-// getAuctions();
-//
-
 const cheerio = require("cheerio");
 const { Builder, By, Key, until } = require("selenium-webdriver");
 const driver = new Builder().forBrowser("chrome").build();
 
-const getAuctions = async () => {
+const getHomePageHTML = async () => {
   try {
     await driver.get("http://bidfta.bidqt.com/BidFTA/#/Main");
     await driver.wait(until.elementLocated(By.className("app-list-item")));
     const html = await driver.getPageSource();
-    const $ = cheerio.load(html);
-
-    const ids = $(".app-list-item")
-      .map((i, el) => {
-        const link = $(el)
-          .find(".app-card-footer a")
-          .attr("href");
-        const [_, id] = link.split("?");
-        return id;
-      })
-      .get();
-
     driver.quit();
 
-    return ids;
+    return html;
   } catch (e) {
     driver.quit();
-    console.error(e);
+
     return e;
   }
+};
+
+const getAuctionIds = html => {
+  const $ = cheerio.load(html);
+  return $(".app-list-item")
+    .map((i, el) => {
+      const link = $(el)
+        .find(".app-card-footer a")
+        .attr("href");
+      const [_, id] = link.split("?");
+      return id;
+    })
+    .get();
+};
+
+const getAuctions = async () => {
+  const html = await getHomePageHTML();
+  const ids = getAuctionIds(html);
+
+  console.log(ids);
+  return ids;
 };
 
 getAuctions();
